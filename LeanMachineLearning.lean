c6c4358dab48f01bb90600c41369d77dcc0701b2
ISIPINK <isidoorp@gmail.com>
2026-09-15 15:08:04 +0200

    added fenchel conjugates

--- LeanMachineLearning.lean
+++ LeanMachineLearning.lean
@@ -1,6 +1,8 @@
 module  -- shake: keep-all --deprecated_module: ignore
 
 public import LeanMachineLearning.ForMathlib.ConvexAnalysis.Bregman.Basic
+public import LeanMachineLearning.ForMathlib.ConvexAnalysis.FenchelConjugate.Basic
+public import LeanMachineLearning.ForMathlib.ConvexAnalysis.FenchelConjugate.Calculus
 public import LeanMachineLearning.ForMathlib.ConvexAnalysis.Subgradient.Basic
 public import LeanMachineLearning.ForMathlib.InformationTheory.KullbackLeibler.ChainRule
 public import LeanMachineLearning.ForMathlib.InformationTheory.KullbackLeibler.CompProd
